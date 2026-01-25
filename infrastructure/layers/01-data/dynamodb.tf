d3b3f8bd36ab849b6b04942d97e8417f42be68b6
Dmytro Kucherenko <kucherenko.dmitriy@gmail.com>
2026-01-25 16:06:07 +0100

    fix: force update to sync missing terraform outputs

--- infrastructure/layers/01-data/dynamodb.tf
+++ infrastructure/layers/01-data/dynamodb.tf
@@ -147,6 +147,7 @@ resource "aws_dynamodb_table" "demo_requests" {
     {
       Name    = "${var.project_name}-demo-requests"
       Purpose = "Store demo/contact form submissions"
+      ForceUpdate = "true"
     }
   )
 }

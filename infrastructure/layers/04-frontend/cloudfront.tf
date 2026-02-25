8829e5017b2276052be86fe2184ccaf3d0815cc4
Dmytro Kucherenko <kucherenko.dmitriy@gmail.com>
2026-02-25 19:23:36 +0100

    fix: adjust formatting in content_security_policy block in cloudfront.tf

--- infrastructure/layers/04-frontend/cloudfront.tf
+++ infrastructure/layers/04-frontend/cloudfront.tf
@@ -72,7 +72,7 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
 
     content_security_policy {
       content_security_policy = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://www.google.com https://www.gstatic.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://*.googleapis.com https://*.gstatic.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://maps.googleapis.com https://www.google.com https://www.google-analytics.com; frame-src https://www.google.com https://www.recaptcha.net; object-src 'none'; base-uri 'self'"
-      override               = true
+      override                = true
     }
   }
 
